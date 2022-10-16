11b5edba6550697a9ec91925a6edfedf9385f99a
Renam Bulhoes <renamgustavo@live.com>
2022-10-15 23:04:38 -0300

    Seed category h2

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO TB_CATEGORY(id, name) VALUES (1, 'Tv');
+INSERT INTO TB_CATEGORY(id, name) VALUES (2, 'Eletronicos')
+INSERT INTO TB_CATEGORY(id, name) VALUES (3, 'Notebooks')
+INSERT INTO TB_CATEGORY(id, name) VALUES (4, 'Maquinas')
