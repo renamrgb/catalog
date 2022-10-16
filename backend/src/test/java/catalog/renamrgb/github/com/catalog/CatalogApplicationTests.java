df411a5bbc60a6bcaea4ead0be988d69ae731ff3
Renam Bulhoes <renamgustavo@live.com>
2022-10-15 21:59:55 -0300

    Projeto inicial

---
+++ backend/src/test/java/catalog/renamrgb/github/com/catalog/CatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package catalog.renamrgb.github.com.catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
